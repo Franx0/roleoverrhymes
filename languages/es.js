382a23440f7dd33704790b4c23531b7643b53867
Franx0 <francisco.moya.martinez@gmail.com>
2022-05-15 19:28:48 +0000

    add genie tale and update footer

--- languages/es.js
+++ languages/es.js
@@ -7,20 +7,17 @@ const es = {
     <p><span class="book title">Role Over Rhymes</span> es un colección de diez rimas alternativas de cuentos populares que incluyen personajes renovados y adaptados, giros argumentales, humor, ambientaciones más actuales y ¡mucha magia! Suzanne Sasse y la ilustradora, June Laurie, actualizan estos cuentos clásicos a los lectores más jovenes..</p> \
   `,
   available: ` \
-    Disponible en:&nbsp; \
-    <span class="link available"><a href=https://www.troubador.co.uk/bookshop/picture-books/role-over-rhymes rel="noreferrer" target="_blank">Troubador</a></span> \
-    &nbsp; \
-    <span class="link available"><a href=https://www.waterstones.com/book/role-over-rhymes/suzanne-sasse/june-laurie/9781803130828 rel="noreferrer" target="_blank">WaterStones</a></span> \
-    &nbsp; \
-    <span class="link available"><a href=https://www.amazon.co.uk/Role-Over-Rhymes-Suzanne-Sasse/dp/1803130822/ref=sr_1_1?crid=1OVXAC088XPDA&keywords=role+over+rhymes&qid=1651523488&sprefix=role+over+rhymes%2Caps%2C200&sr=8-1 rel="noreferrer" target="_blank">Amazon UK</a></span> \
-    &nbsp; \
-    <span class="link available"><a href=https://www.amazon.es/Role-Over-Rhymes-Suzanne-Sasse/dp/1803130822/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3U41N1KJRGDGW&keywords=Role+Over+Rhymes&qid=1651523446&sprefix=role+over+rhyme%2Caps%2C827&sr=8-1 rel="noreferrer" target="_blank">Amazon ES</a></span> \
+    Disponible en: \
+    <span class="md:px-3 link available"><a href=https://www.troubador.co.uk/bookshop/picture-books/role-over-rhymes rel="noreferrer" target="_blank">Troubador</a></span> \
+    <span class="md:px-3 link available"><a href=https://www.waterstones.com/book/role-over-rhymes/suzanne-sasse/june-laurie/9781803130828 rel="noreferrer" target="_blank">WaterStones</a></span> \
+    <span class="md:px-3 link available"><a href=https://www.amazon.co.uk/Role-Over-Rhymes-Suzanne-Sasse/dp/1803130822/ref=sr_1_1?crid=1OVXAC088XPDA&keywords=role+over+rhymes&qid=1651523488&sprefix=role+over+rhymes%2Caps%2C200&sr=8-1 rel="noreferrer" target="_blank">Amazon UK</a></span> \
+    <span class="md:px-3 link available"><a href=https://www.amazon.es/Role-Over-Rhymes-Suzanne-Sasse/dp/1803130822/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3U41N1KJRGDGW&keywords=Role+Over+Rhymes&qid=1651523446&sprefix=role+over+rhyme%2Caps%2C827&sr=8-1 rel="noreferrer" target="_blank">Amazon ES</a></span> \
   `,
   booktrailer: `Booktrailer`,
   gallery: `Galería`,
   genieTale: `The Genie`,
   footer: {
-    handmade: `<span>Diseñado y desarrollado por: </span><span>Ana Mejón&nbsp;y&nbsp<a title="Role Over Rhymes" target="_blank" href=https://github.com/Franx0/roleoverrhymes>&nbsp;Fran Moya</a></span>`
+    handmade: `<span class="flex-1">Diseñado y desarrollado por: </span><span class="flex-2">Ana Mejón&nbsp;y&nbsp<a title="Role Over Rhymes" target="_blank" href=https://github.com/Franx0/roleoverrhymes>Fran Moya</a></span>`
   },
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,23 +35,29 @@ const Home: NextPage<NextPageContext> = (props: any) => {
       </Head>
 
       <main className="flex flex-col px-0 items-center text-2xl font-roboto">
-        <section id="description" className="w-full flex-wrap md:flex-col md:w-full p-16 md:text-justify place-content-center">
+        <section id="description" className="w-full flex-wrap flex-col w-full p-16 md:text-justify place-content-center">
           <div className="max-w-3xl m-auto leading-8" dangerouslySetInnerHTML={{__html: dictionary.description}}></div>
         </section>
-        <section id="trailer" className="w-full flex-wrap md:flex-col md:w-full p-10 text-justify place-content-center bg-roleover">
+        <section id="trailer" className="w-full flex-wrap flex-col w-full p-10 text-justify place-content-center bg-roleover">
           <h2 className="text-center text-white mb-6 font-rancho text-4xl">{dictionary.booktrailer}</h2>
           <iframe className="h-96 w-full m-auto" loading="lazy" width="60%" height="60%" src={bookTrailerVideoSrc} frameBorder="0" allowFullScreen title="Book Trailer"></iframe>
         </section>
-        <section id="photos" className="w-full flex-wrap md:flex-col md:w-full p-16 md:text-justify place-content-center">
+        <section id="photos" className="w-full flex-wrap flex-col w-full p-16 md:text-justify place-content-center">
           <h2 className="text-center text-roleover mb-6 font-rancho text-4xl">{dictionary.gallery}</h2>
           <PhotoSlider styles={{width: "97%"}} slides={photos} />
         </section>
-        <section id="available" className="w-full flex-wrap md:flex-col md:w-full p-10 text-justify place-content-center">
-          <div className="max-w-3xl m-auto" dangerouslySetInnerHTML={{__html: dictionary.available}}></div>
+        <section id="available" className="w-full flex-wrap flex-col w-full p-10 text-justify place-content-center">
+          <div className="flex md:flex-row flex-col md:max-w-3xl md:w-full sm:w-1/2 content-justify content-center text-center m-auto " dangerouslySetInnerHTML={{__html: dictionary.available}}></div>
+        </section>
+        <section id="genie-tale" className="w-full flex-wrap flex-col w-full p-10 text-justify place-content-center bg-complementary">
+          <h2 className="text-center text-white mb-6 font-rancho text-4xl">{dictionary.genieTale}</h2>
+          <div className="flex-wrap md:flex-row flex-col">
+            <iframe className="h-96 w-full" loading="lazy" width="60%" height="60%" src={genieVideoSrc} frameBorder="0" allowFullScreen title="Genie Trailer"></iframe>
+          </div>
         </section>
       </main>
 
-      <footer className="h-content flex border-t p-4 px-5 md:items-center md:justify-between font-roboto" dangerouslySetInnerHTML={{__html: dictionary.footer.handmade}}></footer>
+      <footer className="flex border-t p-4 px-5 md:items-center md:justify-between font-roboto" dangerouslySetInnerHTML={{__html: dictionary.footer.handmade}}></footer>
     </div>
   )
 }
