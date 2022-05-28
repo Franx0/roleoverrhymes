3f635eb7aec034a6bd48650f350e54f9ab2f0088
Franx0 <francisco.moya.martinez@gmail.com>
2022-05-28 19:39:59 +0000

    Undo las stars change

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,41 @@ import PositionedImages from '@/components/PositionedImages'
 
 // Import Resources
 import resources from "@/resources/index";
-import Stars from "@/resources/stars";
+
+const Stars = [
+  {
+    url: "/images/star1.png",
+    class: "star-1 top-1 left-4 md:top-20 lg:left-36 animate-pulse-slow-3 z-0"
+  },
+  {
+    url: "/images/star1.png",
+    class: "star-1 right-2 top-56 md:right-48 animate-pulse-slow-4 z-0"
+  },
+  {
+    url: "/images/star2.png",
+    class: "star-2 left-24 bottom-4 md:left-80 2xl:top-80 animate-pulse z-0"
+  },
+  {
+    url: "/images/star2.png",
+    class: "w-4 star-2 bottom-0 lg:top-96 right-10 md:right-24 xl:right-60 animate-pulse z-0"
+  },
+  {
+    url: "/images/star2.png",
+    class: "star-2 top-44 right-36 lg:top-24 lg:right-80 animate-pulse z-0"
+  },
+  {
+    url: "/images/star3.png",
+    class: "star-3 top-96 lg:top-72 left-0 lg:left-12 animate-pulse z-0"
+  },
+  {
+    url: "/images/star4.png",
+    class: "w-10 star-4 top-3 left-36 lg:left-96 animate-pulse-slow-3 z-0"
+  },
+  {
+    url: "/images/star4.png",
+    class: "star-4 top-3 right-10 animate-pulse-slow-4 z-0"
+  }
+];
 
 const Home: NextPage<NextPageContext> = (props: any) => {
   const { dictionary }: { dictionary: any } = props;
